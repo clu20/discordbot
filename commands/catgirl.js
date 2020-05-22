f8d65382ce6764e6f95859558386385eb609f8f7
Calvin <clu20@ucsc.edu>
2020-05-22 15:01:34 -0700

    pulls random catgirl image and selects largest possible image that's less than 7MB

--- commands/catgirl.js
+++ commands/catgirl.js
@@ -6,16 +6,31 @@ module.exports = {
 	guildOnly: true,
 	cooldown: 1,
 	async execute(msg, response){
-		let page = Math.floor(Math.random()*(100));
-		const list = await fetch(`https://yande.re/post.json?tags=nekomimi&&page=${page}&&limit=2`).then(response => response.json());
-		console.log();
-		//const [test] = list;
-		//const test = list.;
-		//console.log(test);
+		//around 1500 posts for nekomimi tag select a random one using limit search and page number params
+		let page = Math.floor(Math.random()*1500);
+		let max = -1;
+		//actual number is around 8MB but this is easier to work with
+		const discordmax = 7000000;
+		//api returns list of posts with tag
+		const [list] = await fetch(`https://yande.re/post.json?tags=nekomimi&&page=${page}&&limit=1`).then(response => response.json());
+		//if all other images are larger than 7MB send preview
+		let url = list.preview_url;
+
+		if(list.file_size <= discordmax && list.file_size > max){
+			max = list.file_size;
+			url = list.file_url;
+		}
+		if(list.sample_file_size <= discordmax && list.sample_file_size > max){
+			max = list.sample_file_size;
+			url = list.sample_file_url;
+		}
+		if(list.jpeg_file_size <= discordmax && list.jpeg_file_size > max){
+			max = list.jpeg_file_size;
+			url = list.jpeg_file_size;
+		}
 		console.log(page);
-		//console.log(`https://yande.re/post.json?tags=nekomimi?tags=animalears?page=${page}`);
 		console.log(list);
-		msg.channel.send(list[1].preview_url);
+		msg.channel.send(url);
 		console.log();
 		//const { file } = await fetch('https://aws.random.cat/meow').then(response => response.json());
 		//msg.channel.send(file);
