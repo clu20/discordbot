8b7628a095d8ae13c90eea5b2d0a010aa612e8fd
Calvin <clu20@ucsc.edu>
2020-05-22 14:38:27 -0700

    const list now represents entire list fetched from api call and returns second element's preview_url

--- commands/catgirl.js
+++ commands/catgirl.js
@@ -6,15 +6,17 @@ module.exports = {
 	guildOnly: true,
 	cooldown: 1,
 	async execute(msg, response){
-		let page = Math.floor(Math.random()*(100-0+1));
-		const [ list ] = await fetch(`https://yande.re/post.json?tags=nekomimi&&page=${page}&&limit=1`).then(response => response.json());
+		let page = Math.floor(Math.random()*(100));
+		const list = await fetch(`https://yande.re/post.json?tags=nekomimi&&page=${page}&&limit=2`).then(response => response.json());
+		console.log();
 		//const [test] = list;
 		//const test = list.;
 		//console.log(test);
 		console.log(page);
 		//console.log(`https://yande.re/post.json?tags=nekomimi?tags=animalears?page=${page}`);
 		console.log(list);
-		msg.channel.send(list.preview_url);
+		msg.channel.send(list[1].preview_url);
+		console.log();
 		//const { file } = await fetch('https://aws.random.cat/meow').then(response => response.json());
 		//msg.channel.send(file);
 	}
