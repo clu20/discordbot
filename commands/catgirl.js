7200f67cada17bf8b3cd0b90b053c63321e4a258
Calvin <clu20@ucsc.edu>
2020-05-22 04:26:56 -0700

    catgirls are fetched randomly

--- commands/catgirl.js
+++ commands/catgirl.js
@@ -6,7 +6,16 @@ module.exports = {
 	guildOnly: true,
 	cooldown: 1,
 	async execute(msg, response){
-		const { file } = await fetch('https://aws.random.cat/meow').then(response => response.json());
-		msg.channel.send(file);
+		let page = Math.floor(Math.random()*(100-0+1));
+		const [ list ] = await fetch(`https://yande.re/post.json?tags=nekomimi&&page=${page}&&limit=1`).then(response => response.json());
+		//const [test] = list;
+		//const test = list.;
+		//console.log(test);
+		console.log(page);
+		//console.log(`https://yande.re/post.json?tags=nekomimi?tags=animalears?page=${page}`);
+		console.log(list);
+		msg.channel.send(list.preview_url);
+		//const { file } = await fetch('https://aws.random.cat/meow').then(response => response.json());
+		//msg.channel.send(file);
 	}
 }
