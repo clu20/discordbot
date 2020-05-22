80e4a762bdb1674edf0bdeab1dc327d7044415ac
Calvin <clu20@ucsc.edu>
2020-05-21 23:28:55 -0700

    bot can constantly kick user from voice channel on 30 second timed. Can be worked around by constantly joining VC faster than bot can kick

--- index.js
+++ index.js
@@ -7,8 +7,12 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
 const cooldowns = new Discord.Collection();
+const timeouts = new Discord.Collection();
 
 const {prefix, token} = require('./config.json');
+let timeoutstamp = '';
+let now = Date.now();
+let timeoutAmount = 0;
 
 //grab all client commands
 for (const file of commandFiles){
@@ -19,18 +23,34 @@ for (const file of commandFiles){
 //On member join Voice Channel check to see if member is timed-out from kick cmd
 client.on('voiceStateUpdate', (oldmember, newmember) =>{
 	//checks if user is in a channel and/or was in a channel previously
-	console.log('in voicestate');
+	//console.log('in voicestate');
+	if(timeoutstamp.has(oldmember.member.id)){
+		console.log('in');
+		now = Date.now();
+		const expirationTime2 = timeoutstamp.get(oldmember.member.id) + timeoutAmount;
+
+		if(now < expirationTime2){
+			const timeLeft2 = (expirationTime2-now)/1000;
+			//return msg.reply(`please wait ${timeLeft2.toFixed(1)} more second(s) before reusing \`${command.name}\``);
+			oldmember.member.voice.kick('timedout');
+			console.log(`please wait ${timeLeft2.toFixed(1)} more second(s) before reusing`)
+		}
+	}
+	
+
 	let oldvoice = oldmember.channelID;
 	let newvoice = newmember.channelID;
-	console.log(oldvoice);
-	console.log(newvoice);
+	let member = oldmember.member;
+	//console.log(member.nickname);
+	//console.log(oldvoice);
+	//console.log(newvoice);
 	if(oldvoice != newvoice){
 		if(oldvoice == null){
-			console.log("User Joined!");
+			//console.log("User Joined!");
 		}else if (newvoice == null){
-			console.log("User Left!");
+			//console.log("User Left!");
 		}else{
-			console.log("User swapped Channels");
+			//console.log("User swapped Channels");
 	}
 	}
 })
@@ -60,11 +80,12 @@ client.on('message', msg =>{
 	if(!cooldowns.has(command.name)){
 		cooldowns.set(command.name, new Discord.Collection());
 	}
-	const now = Date.now();
+	now = Date.now();
 	const timestamps = cooldowns.get(command.name);
 	//convert to miliseconds
 	const cooldownAmount = (command.cooldown || 3) * 1000;
 
+	//Cooldown for user attempting to reuse cmd
 	if(timestamps.has(msg.author.id)){
 		const expirationTime = timestamps.get(msg.author.id) + cooldownAmount;
 
@@ -75,8 +96,8 @@ client.on('message', msg =>{
 	}
 	//For first time usage set timeout for author
 	timestamps.set(msg.author.id, now);
+	console.log(timestamps);
 	setTimeout(() => timestamps.delete(msg.author.id), cooldownAmount);
-
 	//execute command if all previous conds satisfied
 	try{
 		command.execute(msg,args);
@@ -84,6 +105,20 @@ client.on('message', msg =>{
 		console.error(error);
 		msg.reply("there was an error complain pls");
 	}
+
+	if(!timeouts.has(command.name)){
+		timeouts.set(command.name, new Discord.Collection());
+	}
+
+	timeoutstamp = timeouts.get(command.name);
+	timeoutAmount = (command.timeout || 3) * 1000;
+	const user = msg.mentions.users.first();
+	timeoutstamp.set(user.id, now);
+	setTimeout(() => timeoutstamp.delete(user.id), timeoutAmount);
+	//console.log(msg.mentions.users.first());
+	console.log(timeoutstamp);
+	console.log(timeoutAmount);
+	console.log(user.id);
 })
 
 //client turned on
