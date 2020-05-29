6b3f548d3ac5a46f6adf8c9c9e05000a8962815d
Jeffrey <jxie9@ucmerced.edu>
2020-05-29 16:51:01 -0700

    added maps and changed order of commands

--- commands/yeet.js
+++ commands/yeet.js
@@ -36,66 +36,193 @@ module.exports = {
             summit: 'https://gfycat.com/unrulyadolescentantarcticgiantpetrel'
         }
 
-        // SMOKES FOR T SIDE MIRAGE
-        if(message.content.includes('yeet mirage jungle')) {
-            message.channel.send(`${message.author}, ` + mirage.jungle);
-        }
-        else if(message.content.includes('yeet mirage ct')) {
-            message.channel.send(`${message.author}, ` + mirage.ct);
-        }
-        else if(message.content.includes('yeet mirage stairs')) {
-            message.channel.send(`${message.author}, ` + mirage.stairs);
-        }
-        else if(message.content.includes('yeet mirage window')) {
-            message.channel.send(`${message.author}, ` + mirage.window);
-        }
-        else if(message.content.includes('yeet mirage cat')) {
-            message.channel.send(`${message.author}, ` + mirage.cat + ' stand on top of the garbage bin and aim at antenna');
-        }
-        else if(message.content.includes('yeet mirage bench')) {
-            message.channel.send(`${message.author}, ` + mirage.bench);
-        }
-        else if(message.content.includes('yeet mirage short')) {
-            message.channel.send(`${message.author}, ` + mirage.short);
-        }
-        else if(message.content.includes('yeet mirage connector')) {
-            message.channel.send(`${message.author}, ` + mirage.connector);
-        }
-        else if(message.content.includes('yeet mirage market window')) {
-            message.channel.send(`${message.author}, ` + mirage.w_market);
-        }
-        else if(message.content.includes('yeet mirage market door')) {
-            message.channel.send(`${message.author}, ` + mirage.d_market);
+        // LIST OF CACHE SMOKES AND MOLLIES
+        var cache = {
+            z: 'https://gfycat.com/fearfuloldirishwolfhound',
+            boost: 'https://gfycat.com/weeklyfaroffguppy',
+            headshot: 'https://gfycat.com/perfumedrewardingcommongonolek',
+            vent: 'https://gfycat.com/vapidwarygeese-globaloffensive-csgo',
+            triple: 'https://gfycat.com/whimsicalcourageousafricangoldencat',
+            ct: 'https://gfycat.com/favorableunselfishaustraliansilkyterrier',
+            default: 'https://gfycat.com/wickedagreeablebonobo'
         }
 
+        // LIST OF INFERNO SMOKES, MOLLIES AND FLASHES
+        var inferno = {
+            banger: 'https://gfycat.com/tautimaginativejunebug-globaloffensive-csgo',
+            coffin: 'https://gfycat.com/alarmingenormousdodobird',
+            arches: 'https://gfycat.com/mediumdelectablecatbird',
+            ct: 'https://gfycat.com/uncomfortableadoredgilamonster',
+            truck: 'https://gfycat.com/selfreliantbriefdrever',
+            library: 'https://gfycat.com/variabledismalbuck',
+            moto: 'https://gfycat.com/graciousdecentanhinga',
+            boiler: 'https://gfycat.com/infinitespecificabalone',
+            quad: 'https://gfycat.com/detailedgratefulgoa',
+            dark: 'https://gfycat.com/gregariousviciousdairycow',
+            pit: 'https://gfycat.com/spitefulorganicbirdofparadise'
+        }
 
-        // SMOKES FOR T SIDE TRAIN
-        if(message.content.includes('yeet train a connector')) {
-            message.channel.send(`${message.author}, ` + train.connector);
+        // LIST OF OVERPASS SMOKES AND MOLLIES 
+        var overpass = {
+            heaven: 'https://gfycat.com/consciouslighticefish',
+            bridge: 'https://gfycat.com/illegalwavycrossbill',
+            truck: 'https://gfycat.com/scratchythriftyalabamamapturtle',
+            ct: 'https://gfycat.com/magnificentchillyangelfish',
+            bank: 'https://gfycat.com/firmalienatedeyas',
+            toxic: 'https://gfycat.com/dearconcreteelver'
         }
-        else if(message.content.includes('yeet train back ivy')) {
-            message.channel.send(`${message.author}, ` + train.back_ivy);
+
+        if(message.content.includes('yeet')) { 
+            // MIRAGE
+            if(message.content.includes('mirage')) {
+                if(message.content.includes('jungle')) {
+                    message.channel.send(`${message.author}, ` + mirage.jungle);
+                }
+                if(message.content.includes('ct')) {
+                    message.channel.send(`${message.author}, ` + mirage.ct);
+                }
+                if(message.content.includes('stairs')) {
+                    message.channel.send(`${message.author}, ` + mirage.stairs);
+                }
+                if(message.content.includes('window')) {
+                    message.channel.send(`${message.author}, ` + mirage.window);
+                }
+                if(message.content.includes('cat')) {
+                    message.channel.send(`${message.author}, ` + mirage.cat + ' stand on top of the garbage bin and aim at antenna');
+                }
+                if(message.content.includes('bench')) {
+                    message.channel.send(`${message.author}, ` + mirage.bench);
+                }
+                if(message.content.includes('short')) {
+                    message.channel.send(`${message.author}, ` + mirage.short);
+                }
+                if(message.content.includes('connector')) {
+                    message.channel.send(`${message.author}, ` + mirage.connector);
+                }
+                if(message.content.includes('market window')) {
+                    message.channel.send(`${message.author}, ` + mirage.w_market);
+                }
+                if(message.content.includes('market door')) {
+                    message.channel.send(`${message.author}, ` + mirage.d_market);
+                }
+            }
+            // TRAIN
+            else if(message.content.includes('train')) {
+                if(message.content.includes('connector')) {
+                    message.channel.send(`${message.author}, ` + train.connector);
+                }
+                if(message.content.includes('back ivy')) {
+                    message.channel.send(`${message.author}, ` + train.back_ivy);
+                }
+                if(message.content.includes('olaf')) {
+                    message.channel.send(`${message.author}, ` + train.olaf);
+                }
+                if(message.content.includes('sandwich')) {
+                    message.channel.send(`${message.author}, ` + train.sandwich);
+                }
+                if(message.content.includes('right')) {
+                    message.channel.send(`${message.author}, ` + train.right);
+                }
+                if(message.content.includes('left')) {
+                    message.channel.send(`${message.author}, ` + train.left);
+                }
+                if(message.content.includes('old bomb')) {
+                    message.channel.send(`${message.author}, ` + train.old_bomb);
+                }
+                if(message.content.includes('cat')) {
+                    message.channel.send(`${message.author}, ` + train.cat);
+                }
+                if(message.content.includes('summit')) {
+                    message.channel.send(`${message.author}, ` + train.summit);
+                }
+            }
+            // CACHE
+            else if(message.content.includes('cache')) {
+                if(message.content.includes('z')) {
+                    message.channel.send(`${message.author}, ` + cache.z);
+                }
+                if(message.content.includes('boost')) {
+                    message.channel.send(`${message.author}, ` + cache.boost);
+                }
+                if(message.content.includes('headshot')) {
+                    message.channel.send(`${message.author}, ` + cache.headshot);
+                }
+                if(message.content.includes('vent')) {
+                    message.channel.send(`${message.author}, ` + cache.vent);
+                }
+                if(message.content.includes('triple')) {
+                    message.channel.send(`${message.author}, ` + cache.triple);
+                }
+                if(message.content.includes('ct')) {
+                    message.channel.send(`${message.author}, ` + cache.ct);
+                }
+                if(message.content.includes('default')) {
+                    message.channel.send(`${message.author}, ` + cache.default);
+                }
+            }
+            // INFERNO
+            else if(message.content.includes('inferno')) {
+                if(message.content.includes('banger')) {
+                    message.channel.send(`${message.author}, ` + inferno.banger);
+                }
+                if(message.content.includes('coffin')) {
+                    message.channel.send(`${message.author}, ` + inferno.coffin);
+                }
+                if(message.content.includes('arches')) {
+                    message.channel.send(`${message.author}, ` + inferno.arches);
+                }
+                if(message.content.includes('ct')) {
+                    message.channel.send(`${message.author}, ` + inferno.ct);
+                }
+                if(message.content.includes('truck')) {
+                    message.channel.send(`${message.author}, ` + inferno.truck);
+                }
+                if(message.content.includes('library')) {
+                    message.channel.send(`${message.author}, ` + inferno.library);
+                }
+                if(message.content.includes('moto')) {
+                    message.channel.send(`${message.author}, ` + inferno.moto);
+                }
+                if(message.content.includes('boiler')) {
+                    message.channel.send(`${message.author}, ` + inferno.boiler);
+                }
+                if(message.content.includes('quad')) {
+                    message.channel.send(`${message.author}, ` + inferno.quad);
+                }
+                if(message.content.includes('dark')) {
+                    message.channel.send(`${message.author}, ` + inferno.dark);
+                }
+                if(message.content.includes('pit')) {
+                    message.channel.send(`${message.author}, ` + inferno.pit);
+                }
+            }
+            // OVERPASS
+            else if(message.content.includes('overpass')) {
+                if(message.content.includes('heaven')) {
+                    message.channel.send(`${message.author}, ` + overpass.heaven);
+                }
+                if(message.content.includes('bridge')) {
+                    message.channel.send(`${message.author}, ` + overpass.bridge);
+                }
+                if(message.content.includes('truck')) {
+                    message.channel.send(`${message.author}, ` + overpass.truck);
+                }
+                if(message.content.includes('ct')) {
+                    message.channel.send(`${message.author}, ` + overpass.ct);
+                }
+                if(message.content.includes('bank')) {
+                    message.channel.send(`${message.author}, ` + overpass.bank);
+                }
+                if(message.content.includes('toxic')) {
+                    message.channel.send(`${message.author}, ` + overpass.toxic);
+                }
+            }
+            else {
+                message.channel.send('try map name again b');
+            }
         }
-        else if(message.content.includes('yeet train olaf')) {
-            message.channel.send(`${message.author}, ` + train.olaf);
+        else {
+            message.channel.send('try full command again b');
         }
-        else if(message.content.includes('yeet train sandwich')) {
-            message.channel.send(`${message.author}, ` + train.sandwich);
-        }
-        else if(message.content.includes('yeet train right')) {
-            message.channel.send(`${message.author}, ` + train.right);
-        }
-        else if(message.content.includes('yeet train left')) {
-            message.channel.send(`${message.author}, ` + train.left);
-        }
-        else if(message.content.includes('yeet train old bomb')) {
-            message.channel.send(`${message.author}, ` + train.old_bomb);
-        }
-        else if(message.content.includes('yeet train cat')) {
-            message.channel.send(`${message.author}, ` + train.cat);
-        }
-        else if(message.content.includes('yeet train summit')) {
-            message.channel.send(`${message.author}, ` + train.summit);
-        }
-	},
+    }
 };
