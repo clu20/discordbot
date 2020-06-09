a985c1c8e4569a0cd074145017d2c758e446fc63
Calvin <clu20@ucsc.edu>
2020-06-08 17:52:43 -0700

    Help embedded message updated to send message regardless if a command is missing a field or not. Any desciptors outside of "Description, Usage, Aliases, Cooldown" need to be manually updated. Need to update help.js to dynamically asses what fields to send in help request.

--- commands/help.js
+++ commands/help.js
@@ -22,15 +22,23 @@ module.exports = {
 		}
 
 		data.push(`Name: ${command.name}`);
+		console.log(command.aliases? 'yes':'no');
 		const embed = new Discord.MessageEmbed()
 			.setColor('#EFF00')
 			.setTitle(name)
-			.addFields(
+			/*.addFields(
 				{ name: 'Aliases', value: `${command.aliases.join(', ') || ' '}`},
 				{ name: 'Description', value: `${command.description || ' '}`},
 				{ name: 'Usage', value: `${prefix}${command.name} ${command.usage}` },
 				{ name: 'Cooldown', value: `${command.cooldown || 3} second(s)`}
 
+			);*/
+			.addFields(
+				//cooldown way or picking and string literal evaluate the same way, keeping both as reference
+				{ name: 'Description', value: command.description? `${command.description}` : 'n/a'},
+				{ name: 'Usage', value: `${prefix}${command.name} ${command.usage || ' '}`},
+				{ name: 'Aliases', value: command.aliases? `${command.aliases.join(', ') || ' '}` : 'n/a'},
+				{ name: 'Cooldown', value: `${command.cooldown || 3} second(s)`}
 			);
 		if (command.aliases) data.push(`Aliases: ${command.aliases.join(', ')}`);
 		if (command.description) data.push(`Description: ${command.description}`);
